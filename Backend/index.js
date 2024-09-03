f1b53d87263138a4a6331bd94d1c5e15c5aad1ad
Gnyaneshwari22 <gnyaneshwariadki2@gmail.com>
2024-09-03 16:54:45 +0530

    deploment code

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
@@ -19,10 +19,10 @@ const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGODB_URI;
 
 try {
-    mongoose.connect(URI);
-    console.log("Connected to MongoDB");
+  mongoose.connect(URI);
+  console.log("Connected to MongoDB");
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
 //routes
@@ -30,5 +30,16 @@ app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
 server.listen(PORT, () => {
-    console.log(`Server is Running on port ${PORT}`);
+  console.log(`Server is Running on port ${PORT}`);
 });
+
+//-----------------code for deployment-------------------
+
+if (process.env.NODE_ENV === "production") {
+  const dirPath = path.resolve();
+
+  app.use(express.static("./Frontend/dist"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
+  });
+}
